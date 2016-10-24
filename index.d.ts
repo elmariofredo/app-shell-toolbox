c2ac1a41dc4613bcfe2f971f8cc13c7352f202aa
Mario Vejlupek <mario@vejlupek.cz>
2016-10-25 00:06:06 +0200

    feat(init): add replace string __webpack_public_path__

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+declare global {
+    interface Window {
+        __AppShell_publicPath__: string;
+    }
+}
+
+export { };
---
+++ init.js
@@ -0,0 +1,2 @@
+// Webpack variable to inherit publicPath from App Shell
+__webpack_public_path__ = window.__AppShell_publicPath__;
